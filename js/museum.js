b512cd5b281112727a1eb5b19c9398a26bfd1754
Kristian <kstavem@gmail.com>
2020-09-05 09:23:02 +0200

    navigation complete including js

--- js/museum.js
+++ js/museum.js
@@ -1,19 +1,30 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-    var accordion = document.getElementsByClassName("button__nav--sm");
-    var i;
 
+
+var accordion = document.getElementsByClassName("button__nav--sm");
+var i;
+
+function onlyOne() {
     for (i = 0; i < accordion.length; i++) {
-        accordion[i].addEventListener("click", function () {
-            this.classList.toggle("button--accordion");
-            this.parentElement.classList.toggle("nav--alpha");
-            var collapse = this.nextElementSibling;
-            if (collapse.style.maxHeight) {
-                collapse.style.maxHeight = null;
-            } else {
-                collapse.style.maxHeight = collapse.scrollHeight + "px";
-            }
-        });
+        accordion[i].classList.remove("button--accordion");
+        accordion[i].parentElement.classList.remove("nav--alpha");
+        accordion[i].nextElementSibling.style.maxHeight = null;
     }
-});
+}
+
+for (i = 0; i < accordion.length; i++) {
+    accordion[i].addEventListener("click", function () {
+        onlyOne();
+        var isOpen = this.classList.contains("button--accordion");
+        isOpen ? this.classList.remove("button--accordion") : this.classList.add("button--accordion");
+        this.parentElement.classList.toggle("nav--alpha");
+        var collapse = this.nextElementSibling;
+        if (collapse.style.maxHeight) {
+            collapse.style.maxHeight = null;
+        } else {
+            collapse.style.maxHeight = collapse.scrollHeight + "px";
+        }
+    });
+}
+
 
 
